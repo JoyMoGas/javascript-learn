266aa0d021459f15230565bfcbbd971cf6cfe833
JoyMoGas <joseantoniomontanogastelum5@gmail.com>
2024-05-09 16:31:22 -0700

    Colores RBG Finished

---
+++ .history/colores-regb-input/app_20240509162122.js
@@ -0,0 +1,11 @@
+const inputRojo = document.getElementById('rojo');
+const inputVerde = document.getElementById('verde');
+const inputAzul = document.getElementById('azul');
+
+const textoRojo = document.getElementById('texto-rojo');
+const textoVerde = document.getElementById('texto-verde');
+const textoAzul = document.getElementById('texto-azul');
+
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
---
+++ .history/colores-regb-input/app_20240509162228.js
@@ -0,0 +1,17 @@
+const inputRojo = document.getElementById('rojo');
+const inputVerde = document.getElementById('verde');
+const inputAzul = document.getElementById('azul');
+
+const textoRojo = document.getElementById('texto-rojo');
+const textoVerde = document.getElementById('texto-verde');
+const textoAzul = document.getElementById('texto-azul');
+
+let rojo = inputRojo.value;
+let verde = inputVerde.value;
+let azul = inputAzul.value;
+
+// Actualizar texto de parrafos
+textoRojo.innerText = rojo;
+textoVerde.innerText = verde;
+textoAzul.innerText = azul;
+
